71daecb2a6d2b66baef2f7a2bf2670b2340cd1b0
Braden Peterson <peterson.braden2@gmail.com>
2026-02-24 11:39:29 -0700

    Added bsp controller

--- scripts/Controllers/BinaryController.cs
+++ scripts/Controllers/BinaryController.cs
@@ -3,13 +3,40 @@ using System;
 
 public partial class BinaryController : Node
 {
-	// Called when the node enters the scene tree for the first time.
+	[ExportGroup("Generation Parameters")]
+	[Export] public int Width { get; set; } = 100;
+	[Export] public int Height { get; set; } = 100;
+	[Export] public int MinDepth { get; set; } = 1;
+	[Export] public int MaxDepth { get; set; } = 10;
+	[Export] public float SplitChance { get; set; } = 0.5f;
+	[Export] public int Seed { get; set; } = 0;
+
+	private BinarySpacePartitioningRenderer _renderer;
+	private CameraController _camera;
+
 	public override void _Ready()
 	{
-	}
+		var parent = GetParent();
+		_renderer = parent?.GetNodeOrNull<BinarySpacePartitioningRenderer>("BinarySpacePartitioningRenderer");
+		_camera = parent?.GetNodeOrNull<CameraController>("CameraController");
+	
+		if (_renderer == null)
+		{
+			GD.PrintErr("BinaryController: Could not find BinarySpacePartitioningRenderer in scene tree");
+			return;
+		}
+		
+		if (_camera != null)
+			_renderer.GridGenerated += _camera.CenterCameraOnGrid;
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
+		Regenerate();
 	}
+	
+	public void Regenerate()
+	{
+		int[,] grid = BinarySpacePartitioningGenerator.Generate(Width, Height, MinDepth, MaxDepth, SplitChance, Seed > 0 ? Seed : (int?)null);
+		_renderer.Render(grid);
+	}
+	
+	
 }
